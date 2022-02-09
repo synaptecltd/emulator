4e62244092e2996133cf77c5505efa1787b903d5
Jason Costello <jason.costello@synapt.ec>
2022-02-09 15:17:55 +0000

    Reverting emulator test fixture used for benchmarking

--- emulator_test.go
+++ emulator_test.go
@@ -6,6 +6,38 @@ import (
 	"testing"
 )
 
+// benchmark emulator performance
+func BenchmarkEmulator(b *testing.B) {
+	emu := createEmulatorForBenchmark(4000, 0)
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 4000; j++ {
+			emu.Step()
+		}
+	}
+}
+
+
+func createEmulatorForBenchmark(samplingRate int, phaseOffsetDeg float64) *Emulator {
+	emu := NewEmulator(samplingRate, 50.0)
+
+	emu.V = &ThreePhaseEmulation{
+		PosSeqMag:   400000.0 / math.Sqrt(3) * math.Sqrt(2),
+		NoiseMax:    0.000001,
+		PhaseOffset: phaseOffsetDeg * math.Pi / 180.0,
+	}
+	emu.I = &ThreePhaseEmulation{
+		PosSeqMag:       500.0,
+		PhaseOffset:     phaseOffsetDeg * math.Pi / 180.0,
+		HarmonicNumbers: []float64{5, 7, 11, 13, 17, 19, 23, 25},
+		HarmonicMags:    []float64{0.2164, 0.1242, 0.0892, 0.0693, 0.0541, 0.0458, 0.0370, 0.0332},
+		HarmonicAngs:    []float64{171.5, 100.4, -52.4, 128.3, 80.0, 2.9, -146.8, 133.9},
+		NoiseMax:        0.000001,
+	}
+
+	return emu
+}
+
 func createEmulator(samplingRate int, phaseOffsetDeg float64) *Emulator {
 	emu := NewEmulator(samplingRate, 50.0)
 
@@ -31,16 +63,7 @@ func createEmulator(samplingRate int, phaseOffsetDeg float64) *Emulator {
 	return emu
 }
 
-// benchmark emulator performance
-func BenchmarkEmulator(b *testing.B) {
-	emu := createEmulator(4000, 0)
 
-	for i := 0; i < b.N; i++ {
-		for j := 0; j < 4000; j++ {
-			emu.Step()
-		}
-	}
-}
 
 func FloatingPointEqual(expected float64, actual float64, threshold float64) bool {
 	absDiff := math.Abs(expected - actual)
